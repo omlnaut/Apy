137a62229dd73a1fd8fc531825f9c82f8f79375b
omlnaut <oneironaut.oml@gmail.com>
2025-09-14 19:59:05 +0000

    include router

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from app.api.ItemRouter import router as item_router
 
 app = FastAPI()
+
+app.include_router(item_router, prefix="/items", tags=["items"])
 
 
 @app.get("/")
